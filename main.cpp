c247a0506847d1afc8b6b4c4a58ac329ac84329e
GustavNK <gustav.knudsen96@gmail.com>
2020-04-22 13:50:10 +0200

    init

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+ * Receiver.cpp
+ *
+ * Created: 22-04-2020 13:41:23
+ * Author : Gustav
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+		int x = 2;
+    }
+}
+
